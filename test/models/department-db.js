c5732805240387b02c764b75ec6db36e251f6aed
Mitch Kroska <kromitj@gmail.com>
2019-03-04 01:54:22 -0700

    Refactor seedData

--- test/models/department-db.js
+++ test/models/department-db.js
@@ -12,54 +12,54 @@ const app = require("../../app")
 const { Department } = require('../../models')
 
 describe('Department to DB', () => {
-		it("can find the first record", (done) => {
-			Department.findByPk(1)
-			.then(department => {
-			  expect(department.get('name')).to.equal('Regional')		
-			  done()	  
-			})
-			.catch((err) => {
-				done(err)
+	it("can find the first record", (done) => {
+		Department.findByPk(1)
+		.then(department => {
+		  expect(department.get('name')).to.equal('Regional')		
+		  done()	  
+		})
+		.catch((err) => {
+			done(err)
+		})
+	})
+	it("can create a new Department and save it to the db", (done) => {
+		Department.create(departmentSeed)
+		.then((department) => {
+			expect(department.get("name")).to.equal(departmentSeed.name)
+			expect(department.get("description")).to.equal(departmentSeed.description)
+			done()
+		})
+		.catch((err) => {
+			done(err)
+		}).then(() => {
+			Department.destroy({
+				where: {
+					name: departmentSeed.name
+				}
+			}).catch((err) => {
+				console.error(err)
 			})
 		})
-		it("can create a new Department and save it to the db", (done) => {
-			Department.create(departmentSeed)
-			.then((department) => {
-				expect(department.get("name")).to.equal(departmentSeed.name)
-				expect(department.get("description")).to.equal(departmentSeed.description)
-				done()
-			})
-			.catch((err) => {
-				done(err)
-			}).then(() => {
-				Department.destroy({
-					where: {
-						name: departmentSeed.name
-					}
-				}).catch((err) => {
-					console.error(err)
-				})
+	})
+	describe("Not-nullable properties give the proper validation errors", () => {
+		notNullAbles.forEach((property) => {
+			it(`Error thrown for ${property}`, (done) => {
+				departmentSeed[property] = null
+				Department.create(departmentSeed).catch((err) => {
+					expect(err.name).to.equal('SequelizeValidationError')
+					done()
+				})				
 			})
 		})
-		describe("Not-nullable properties give the proper validation errors", () => {
-			notNullAbles.forEach((property) => {
-				it(`Error thrown for ${property}`, (done) => {
-					departmentSeed[property] = null
-					Department.create(departmentSeed).catch((err) => {
-						expect(err.name).to.equal('SequelizeValidationError')
-						done()
-					})				
-				})
-			})
-		})	
-		describe("Assossiations: ", () => {
-			it("can find its associated category", (done) => {			
-				Department.findByPk(1).then((department) => {
-					department.getCategories().then((categories) => {
-						expect(categories[0].name).to.equal("French")
-						done()
-					}).catch((err) => 	done(err))
+	})	
+	describe("Assossiations: ", () => {
+		it("can find its associated category", (done) => {			
+			Department.findByPk(1).then((department) => {
+				department.getCategories().then((categories) => {
+					expect(categories[0].name).to.equal("French")
+					done()
 				}).catch((err) => 	done(err))
-			})
-		})		
+			}).catch((err) => 	done(err))
+		})
+	})		
 }) 
