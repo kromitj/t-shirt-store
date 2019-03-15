0453ad4fedc093812b75771eabff28ee03fecb7c
Mitch Kroska <kromitj@gmail.com>
2019-03-15 01:18:06 -0600

    Deploy to Heroku without forest in app.js

--- models/index.js
+++ models/index.js
@@ -15,7 +15,7 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(DB_DB, DB_USER, DB_PASS, {host: DB_HOST, dialect: 'mysql'})
+  sequelize = new Sequelize(DB_DB, DB_USER, DB_PASS, {host: DB_HOST, dialect: 'mysql', dialectOptions: {  ssl: true}})
   sequelize.authenticate()
   .then(() => {
     console.log('Connection has been established successfully.');
@@ -45,3 +45,4 @@ db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 module.exports = db;
+dialectOptions: {  ssl: true}
