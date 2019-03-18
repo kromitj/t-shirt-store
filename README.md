2e40efc3529685d77994b69bddb96270dd26e950
Mitch Kroska <kromitj@gmail.com>
2019-03-17 23:11:43 -0600

    Start documentation

--- README.md
+++ README.md
@@ -1,8 +1,21 @@
 # t-shirt-store
+Developer Set-Up Steps:
+1. npm client-install
+2. npm client-build
+3. npm install
+4 npm run test
+5 npm start
 
 ##Schema
 ![Pic of Schema](./Schema.png "Schema")
 
 ##Mobile Landing Page
-
 ![Pic of Schema](./WireFrames/Mobile-Landing.png "Schema")
+
+##About Aplication:
+* Uses MERN stack except mySQL instead of MongoDB.
+* Pictures are hosted on cloudinary
+* MySQL DB is hosted on ScaleGrid
+* Front-end uses React with Redux
+* Cart information is stored on users localstorage using their ipAdress + 'tShirtStore' as the key
+* Once a user logs in a jwtToken is saved on their localStorage and is good for 1 hour
--- app.js
+++ app.js
@@ -1,31 +1,24 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const requestIp = require('request-ip');
 
 
 
 const dotenv = require('dotenv');
 	dotenv.config();
-var indexRouter = require('./routes/index');
+const indexRouter = require('./routes/index');
 const productRouter = require('./routes/product');
 const customerRouter = require('./routes/customer')
 const clientRouter = require('./routes/client')
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(requestIp.mw())
-
- // app.use(require('forest-express-sequelize').init({
- //    modelsDir: __dirname + '/models',
- //    envSecret: process.env.FOREST_ENV_SECRET,
- //    authSecret: process.env.FOREST_AUTH_SECRET,
- //    sequelize: require('./models').sequelize,
- //  }));
 
 // Passport middleware
 const passport = require('passport');
